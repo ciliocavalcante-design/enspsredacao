4ecaa851f38f06fbf589005dd12fa4f3974d71cc
ciliocavalcante-design <ciliocavalcante@gmail.com>
2026-04-15 15:47:56 -0300

    Update redacoes.js

--- js/redacoes.js
+++ js/redacoes.js
@@ -4,254 +4,508 @@ const redacoes = [
         id: 1,
         aluno: "ADRYEL LEITE RODRIGUES NUNES VIANA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "ADRYEL LEITE RODRIGUES NUNES VIANA.JPG",
+        imagem: "0001.JPG",
         data: "23/03/2026"
     },
     {
         id: 2,
-        aluno: "ARIEL MELO PATRICIO - 1º",
+        aluno: "ANA LIVIA NASCIMENTO FEITOSA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "ARIEL MELO PATRICIO.JPG",
+        imagem: "0002.JPG",
         data: "23/03/2026"
     },
     {
         id: 3,
-        aluno: "DAVIH MARIANO BARROS DE OLIVEIRA - 1º",
+        aluno: "ARIEL MELO PATRICIO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "DAVIH MARIANO BARROS DE OLIVEIRA.JPG",
+        imagem: "0003.JPG",
         data: "23/03/2026"
     },
     {
         id: 4,
-        aluno: "FERNANDA VIANA SA TEIXEIRA - 1º",
+        aluno: "DAVIH MARIANO BARROS DE OLIVEIRA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "FERNANDA VIANA SA TEIXEIRA.JPG",
+        imagem: "0004.JPG",
         data: "23/03/2026"
     },
     {
         id: 5,
-        aluno: "ISADORA REBOUCAS MILITAO - 1º",
+        aluno: "FERNANDA VIANA SA TEIXEIRA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "ISADORA REBOUCAS MILITAO.JPG",
+        imagem: "0005.JPG",
         data: "23/03/2026"
     },
     {
         id: 6,
-        aluno: "JAMILLY VITORIA RIBEIRO SILVA - 1º",
+        aluno: "ISADORA REBOUÇAS MILITÃO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "JAMILLY VITORIA RIBEIRO SILVA.JPG",
+        imagem: "0006.JPG",
         data: "23/03/2026"
     },
     {
         id: 7,
-        aluno: "JOAO VITOR MAIA ARAGAO - 1º",
+        aluno: "JAMILLY VITORIA RIBEIRO SILVA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "JOAO VITOR MAIA ARAGAO.JPG",
+        imagem: "0007.JPG",
         data: "23/03/2026"
     },
     {
         id: 8,
-        aluno: "JOSE THALYS FRAGA OLIVEIRA - 1º",
+        aluno: "JOAO VITOR MAIA ARAGAO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "JOSE THALYS FRAGA OLIVEIRA.JPG",
+        imagem: "0008.JPG",
         data: "23/03/2026"
     },
     {
         id: 9,
-        aluno: "KAYO JHEBERSON MAGALHAES SANTIAGO - 1º",
+        aluno: "JOSE THALYS FRAGA OLIVEIRA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "KAYO JHEBERSON MAGALHAES SANTIAGO.JPG",
+        imagem: "0009.JPG",
         data: "23/03/2026"
     },
     {
         id: 10,
-        aluno: "KLYSSIA DA SILVA BORGES - 1º",
+        aluno: "KAYO JHEBERSON MAGALHAES SANTIAGO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "KLYSSIA DA SILVA BORGES.JPG",
+        imagem: "0010.JPG",
         data: "23/03/2026"
     },
     {
         id: 11,
-        aluno: "LARA CHRISTINE LIMA SALLES - 1º",
+        aluno: "KLYSSIA DA SILVA BORGES - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "LARA CHRISTINE LIMA SALLES.JPG",
+        imagem: "0011.JPG",
         data: "23/03/2026"
     },
     {
         id: 12,
-        aluno: "MAILY EVELY DOS SANTOS RIBEIRO - 1º",
+        aluno: "LARA CHRISTINE LIMA SALLES - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "MAILY EVELY DOS SANTOS RIBEIRO.JPG",
+        imagem: "0012.JPG",
         data: "23/03/2026"
     },
     {
         id: 13,
-        aluno: "MARCIO WELLINGTON DA COSTA FILHO - 1º",
+        aluno: "MAILY EVELY DOS SANTOS RIBEIRO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "MARCIO WELLINGTON DA COSTA FILHO.JPG",
+        imagem: "0013.JPG",
         data: "23/03/2026"
     },
     {
         id: 14,
-        aluno: "MARIA ISABEL DO NASCIMENTO BARBOSA - 1º",
+        aluno: "MARCIO WELLINGTON DA COSTA FILHO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "MARIA ISABEL DO NASCIMENTO BARBOSA.JPG",
+        imagem: "0014.JPG",
         data: "23/03/2026"
     },
     {
         id: 15,
-        aluno: "MARIA VITORIA BERNARDINO AGUIAR - 1º",
+        aluno: "MARIA ISABEL DO NASCIMENTO BARBOSA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "MARIA VITORIA BERNARDINO AGUIAR.JPG",
+        imagem: "0015.JPG",
         data: "23/03/2026"
     },
     {
         id: 16,
-        aluno: "PEDRO VINICIUS BARROS LIMA - 1º",
+        aluno: "MARIA VITORIA BERNARDINO AGUIAR - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "PEDRO VINICIUS BARROS LIMA.JPG",
+        imagem: "0016.JPG",
         data: "23/03/2026"
     },
     {
         id: 17,
-        aluno: "ROBERT DOUGLAS SAAD RODRIGUES FILHO - 1º",
+        aluno: "PEDRO VINICIUS BARROS LIMA - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "ROBERT DOUGLAS SAAD RODRIGUES FILHO.JPG",
+        imagem: "0017.JPG",
         data: "23/03/2026"
     },
     {
         id: 18,
-        aluno: "SARA GABRIELE SILVA GOMES - 1º",
+        aluno: "ROBERT DOUGLAS SAAD RODRIGUES FILHO - 1º",
         tema: "Proposta 1º Ano",
-        imagem: "SARA GABRIELE SILVA GOMES.JPG",
+        imagem: "0018.JPG",
         data: "23/03/2026"
     },
+    {
+        id: 19,
+        aluno: "SARA GABRIELE SILVA GOMES - 1º",
+        tema: "Proposta 1º Ano",
+        imagem: "0019.JPG",
+        data: "23/03/2026"
+    }
 
     // 2º ANO
     {
-        id: 19,
+        id: 20,
         aluno: "ANA NICOLLY MEDEIROS MAIA - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "ANA NICOLLY MEDEIROS MAIA.JPG",
-        data: "23/03/2026"
-    },
-    {
-        id: 20,
-        aluno: "ANITA GABRIELE SOARES MAGALHAES - 2º",
-        tema: "Proposta 2º Ano",
-        imagem: "ANITA GABRIELE SOARES MAGALHAES.JPG",
+        imagem: "0020.JPG",
         data: "23/03/2026"
     },
     {
         id: 21,
-        aluno: "BEATRIZ SOLANGE CHAVES MAGALHAES - 2º",
+        aluno: "ANGELO VINICIUS LEITE SILVA FILHO - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "BEATRIZ SOLANGE CHAVES MAGALHAES.JPG",
+        imagem: "0021.JPG",
         data: "23/03/2026"
     },
     {
         id: 22,
-        aluno: "CAMILA MATOS NASCIMENTO - 2º",
+        aluno: "ANITA GABRIELE SOARES MAGALHAES - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "CAMILA MATOS NASCIMENTO.JPG",
+        imagem: "0022.JPG",
         data: "23/03/2026"
     },
     {
         id: 23,
-        aluno: "JAMES ALAN BARROS ALVES CAVALCANTE - 2º",
+        aluno: "BEATRIZ SOLANGE CHAVES MAGALHAES - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "JAMES ALAN BARROS ALVES CAVALCANTE.JPG",
+        imagem: "0023.JPG",
         data: "23/03/2026"
     },
     {
         id: 24,
-        aluno: "JOAO HENRIQUE FERNANDES PEREIRA - 2º",
+        aluno: "CAMILA MATOS NASCIMENTO - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "JOAO HENRIQUE FERNANDES PEREIRA.JPG",
+        imagem: "0024.JPG",
         data: "23/03/2026"
     },
     {
         id: 25,
-        aluno: "JUAN DAVY SOARES MACIEL - 2º",
+        aluno: "JAMES ALAN BARROS ALVES CAVALCANTE - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "JUAN DAVY SOARES MACIEL.JPG",
+        imagem: "0025.JPG",
         data: "23/03/2026"
     },
     {
         id: 26,
-        aluno: "LARISSA DIAS DUARTE - 2º",
+        aluno: "JOAO HENRIQUE FERNANDES PEREIRA - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "LARISSA DIAS DUARTE.JPG",
+        imagem: "0026.JPG",
         data: "23/03/2026"
     },
     {
         id: 27,
-        aluno: "LARISSA DOS SANTOS ALCANTARA - 2º",
+        aluno: "JUAN DAVY SOARES MACIEL - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "LARISSA DOS SANTOS ALCANTARA.JPG",
+        imagem: "0027.JPG",
         data: "23/03/2026"
     },
     {
         id: 28,
-        aluno: "LISLEY KATLYN MARTINS - 2º",
+        aluno: "KAUE NICOLAS FELIX NICOLAU - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "LISLEY KATLYN MARTINS.JPG",
+        imagem: "0028.JPG",
         data: "23/03/2026"
     },
     {
         id: 29,
-        aluno: "LIVIA IRIS RODRIGUES DE CASTRO - 2º",
+        aluno: "LAILA RIOS BRILHANTE - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "LIVIA IRIS RODRIGUES DE CASTRO.JPG",
+        imagem: "0029.JPG",
         data: "23/03/2026"
     },
     {
         id: 30,
-        aluno: "MARIA CLARA CAVALCANTE FRAGA - 2º",
+        aluno: "LANA NUNES DO NASCIMENTO - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "MARIA CLARA CAVALCANTE FRAGA.JPG",
+        imagem: "0030.JPG",
         data: "23/03/2026"
     },
     {
         id: 31,
-        aluno: "MARIANA DE FATIMA DE MATOS MESQUITA - 2º",
+        aluno: "LARISSA DIAS DUARTE - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "MARIANA DE FATIMA DE MATOS MESQUITA.JPG",
+        imagem: "0031.JPG",
         data: "23/03/2026"
     },
     {
         id: 32,
-        aluno: "MOISES SOARES PESSOA NETO - 2º",
+        aluno: "LARISSA DOS SANTOS ALCANTARA - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "MOISES SOARES PESSOA NETO.JPG",
+        imagem: "0032.JPG",
         data: "23/03/2026"
     },
     {
         id: 33,
-        aluno: "SAMUEL ANTONIO DE ABREU ALVES VIEIRA - 2º",
+        aluno: "LISLEY KATLYN MARTINS - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "SAMUEL ANTONIO DE ABREU ALVES VIEIRA.JPG",
+        imagem: "0033.JPG",
         data: "23/03/2026"
     },
     {
         id: 34,
-        aluno: "SAMUEL FERNANDES DE OLIVEIRA - 2º",
+        aluno: "LIVIA IRIS RODRIGUES DE CASTRO - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "SAMUEL FERNANDES DE OLIVEIRA.JPG",
+        imagem: "0034.JPG",
         data: "23/03/2026"
     },
     {
         id: 35,
-        aluno: "SAYONARA EMILLY PEREIRA NERI - 2º",
+        aluno: "MARIA CLARA CAVALCANTE FRAGA - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "SAYONARA EMILLY PEREIRA NERI.JPG",
+        imagem: "0035.JPG",
         data: "23/03/2026"
     },
     {
         id: 36,
+        aluno: "MARIA JULIA DE ARAUJO CAVALCANTE - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0036.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 37,
+        aluno: "MARIANA DE FATIMA DE MATOS MESQUITA - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0037.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 38,
+        aluno: "MOISES SOARES PESSOA NETO - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0038.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 39,
+        aluno: "NICOLLY SOUZA RABAY - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0039.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 40,
+        aluno: "SAMUEL ANTONIO DE ABREU ALVES VIEIRA - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0040.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 41,
+        aluno: "SAMUEL FERNANDES DE OLIVEIRA - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0041.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 42,
+        aluno: "SAYONARA EMILLY PEREIRA NERI - 2º",
+        tema: "Proposta 2º Ano",
+        imagem: "0042.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 43,
         aluno: "YAGO SA DE OLIVEIRA - 2º",
         tema: "Proposta 2º Ano",
-        imagem: "YAGO SA DE OLIVEIRA.JPG",
+        imagem: "0043.JPG",
+        data: "23/03/2026"
+    }
+
+    // 3º ANO
+    {
+        id: 44,
+        aluno: "ANA LETICIA GOMES FREIRE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0044.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 45,
+        aluno: "ANNA CAROLINA QUEIROZ DE ARAUJO - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0045.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 46,
+        aluno: "ARTHUR SILVA CAVALCANTE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0046.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 47,
+        aluno: "ARTUR FERREIRA LOPES - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0047.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 48,
+        aluno: "BRUNO VICTOR DE SOUSA CAVALCANTE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0048.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 49,
+        aluno: "CARLOS ALBERTO DOS SANTOS GONÇALVES - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0049.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 50,
+        aluno: "GABRIEL MARTINS ANDRADE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0050.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 51,
+        aluno: "GABRIEL SILVA DE MOURA FREIRE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0051.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 52,
+        aluno: "GUILHERME DOS REIS CAVALCANTI - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0052.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 53,
+        aluno: "HEITOR LIMA MARTINS - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0053.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 54,
+        aluno: "JOAO MARCELO RIOLO DA SILVA - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0054.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 55,
+        aluno: "LUIS FERNANDO CLAUDIO FERRO - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0055.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 56,
+        aluno: "MARCOS VENICIOS SILVA FARIAS - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0056.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 57,
+        aluno: "MARIA CLARA PINTO RODRIGUES - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0057.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 58,
+        aluno: "MARIA FERNANDA DE MESQUITA MENEZES - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0058.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 59,
+        aluno: "MARIA GABRIELA MADEIRO DE ALCANTARA - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0059.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 60,
+        aluno: "MIRELLY EVELIN DA SILVA FERREIRA - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0060.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 61,
+        aluno: "NAYLA ARAUJO DE SOUSA - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0061.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 62,
+        aluno: "OTAVIO MESSIAS PEREIRA GOMES - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0062.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 63,
+        aluno: "PEDRO HENRIQUE NASCIMENTO DE OLIVEIRA - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0063.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 64,
+        aluno: "RAUAN DE AMORIM MONTE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0064.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 65,
+        aluno: "REYNALDO DE ANIFA DOMINGOS FENIASSE - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0065.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 66,
+        aluno: "RICARDO DA SILVA GONÇALVES FILHO - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0066.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 67,
+        aluno: "RODRIGO DOS REIS GONDIM - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0067.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 68,
+        aluno: "RYAN DELMAR SOUSA AUGUSTO ARISTOTELES - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0068.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 69,
+        aluno: "SAULO MESQUITA ARAUJO - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0069.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 70,
+        aluno: "SILVIA CIBELE SILVA LIMA - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0070.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 71,
+        aluno: "YASMIN LIMA FURTADO - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0071.JPG",
+        data: "23/03/2026"
+    },
+    {
+        id: 72,
+        aluno: "YURI PEREIRA BARROSO - 3º",
+        tema: "Proposta 3º Ano",
+        imagem: "0072.JPG",
         data: "23/03/2026"
     }
 ];
